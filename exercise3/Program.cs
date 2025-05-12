726754b682fe336095bd95c353e5f8f7956eb7b4
brunastockett7 <bruna__stockett@outlook.com>
2025-05-11 17:34:09 -0800

    update

---
+++ exercise3/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random randomGenerator = new Random();
+        string playAgain;
+
+        do
+        {
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int attempts = 0;
+
+            Console.WriteLine("Guess the magic number between 1 and 100");
+
+            while (guess != magicNumber && attempts < 3)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            if (guess != magicNumber)
+            {
+                Console.WriteLine($"The magic number was {magicNumber}");
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+        }
+        while (playAgain == "yes");
+    }
+}
